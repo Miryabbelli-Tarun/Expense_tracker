4c804c238803c530750d4dd4ce89e00b33a01ce6
Miryabbelli Tarun <miryabbellitarun@gmail.com>
2025-12-17 22:52:02 +0530

    Add authentication

--- tracker/urls.py
+++ tracker/urls.py
@@ -5,5 +5,8 @@ from tracker import views
 
 urlpatterns = [
     path('',views.index,name='index'),
-    path('delete-history/<int:id>',views.delete_history,name='delete_history')
+    path('delete-history/<int:id>',views.delete_history,name='delete_history'),
+    path('login/',views.login_view,name='login_view'),
+    path('register/',views.register_view,name='register_view'),
+    path('logout/',views.logout_view,name='logout_view'),
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -1,8 +1,59 @@
 from django.shortcuts import redirect, render
 from .models import TrackingHistory,CurrentBalance
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def login_view(request):
+    if request.method=='POST':
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+
+        user=User.objects.filter(username=username)
+        if not user.exists():
+            messages.success(request,"User not exist")
+            return redirect('login_view')
+        user=authenticate(username=username,password=password)
+        if not user:
+            messages.success(request,"incorrect password")
+            return redirect('login_view')
+        login(request,user)
+        return redirect('index')
+    return render(request,'login.html')
+
+def register_view(request):
+    if request.method=='POST':
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        first_name=request.POST.get('first_name')
+        last_name=request.POST.get('last_name')
+
+        user=User.objects.filter(username=username)
+        if user.exists():
+            messages.success(request,"Username is already taken!")
+            return redirect('register_view')
+        user=User.objects.create(
+            username=username,
+            first_name=first_name,
+            last_name=last_name
+        )
+        user.set_password(password)
+        user.save()
+        messages.success(request,"User name is created")
+        return redirect('register_view')
+    return render(request,'register.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login_view')
+
+
+
+
+
+@login_required(login_url='login_view')
 def index(request):
     if request.method=='POST':
         amount=request.POST.get('amount')
